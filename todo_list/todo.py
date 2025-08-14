8646df523e748164807b776657c9b064e3540694
Kevin Galvan <kevin.galvan@live.com>
2025-08-13 18:59:53 -0700

    Implement task addition functionality and create tasks file

--- todo_list/todo.py
+++ todo_list/todo.py
@@ -1,10 +1,14 @@
+# global task list
+tasks = []
+
 def main():
     print("Welcome to the todo app!")
     answer = getUserInput()
     if answer == 'y':
-        print("You selected yes")
+        addTask()
     else:
         print("You selected no")
+        print(tasks)
 
 def getUserInput():
     while True:
@@ -13,6 +17,11 @@ def getUserInput():
             return prompt.lower()
         else:
             print("Invalid input, please enter 'y' or 'n'.")
+
+def addTask():
+    taskDescription = input("Enter the task description: ")
+    tasks.append(taskDescription)
+    print("Task added!")
 
 if __name__ == "__main__":
     main()
